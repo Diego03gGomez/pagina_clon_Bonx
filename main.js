7228b4f486b87eff4b1a471df811a219303a3d2d
Diego <ezequielgomezdie@gmail.com>
2023-08-23 00:50:56 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    
+
+
+$(".icono_menu").click(function (e) { 
+    e.preventDefault();
+    
+$(".fondo_menu").toggleClass("trasladar");
+
+
+});
+
+$(".cerrar").click(function (e) { 
+    e.preventDefault();
+    $(".fondo_menu").toggleClass("trasladar");
+  
+    
+});
+
+
+
+
+
+
+
+
+});
