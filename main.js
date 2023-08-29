896ba5d2781d4e9016c756ded3ac087836da9d05
Diego <ezequielgomezdie@gmail.com>
2023-08-28 21:06:34 -0300

    Agregando contadores

--- main.js
+++ main.js
@@ -29,7 +29,55 @@ $(window).scroll(function () {
         
 })
 */
+$(_ => {
+    let target = parseInt($(".amarillo").text());
+    let current = 0;
+    let interval = setInterval(() => {
+        $(".amarillo").text(current);
+        if (current >= target) {
+            clearInterval(interval);
+        }
+        current++;
+    }, 1);
+});
 
+$(_ => {
+    let target = parseInt($(".celeste").text());
+    let current = 0;
+    let interval = setInterval(() => {
+        $(".celeste").text(current);
+        if (current >= target) {
+            clearInterval(interval);
+        }
+        current++;
+    }, 10);
+});
+
+
+
+$(_ => {
+    let target = parseInt($(".naranja").text());
+    let current = 0;
+    let interval = setInterval(() => {
+        $(".naranja").text(current);
+        if (current >= target) {
+            clearInterval(interval);
+        }
+        current++;
+    }, 10);
+});
+
+$(_ => {
+    let target = parseInt($(".violet2").text());
+    let current = 0;
+    let interval = setInterval(() => {
+        $(".violeta2").text(current);
+        if (current >= target) {
+            clearInterval(interval);
+        }
+        current++;
+    }, 10);
+});
 
 
 
